6aff286e600f7ef16284a949de23b83bf0ce8cc2
DArkSHADOW110 <thilankhewawasam2@gmail.com>
2025-02-19 09:05:29 +0530

    Update second.c

--- second.c
+++ second.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 int main() {
 
-  printf("welcome to the fundementals of programming session")
+  printf("welcome to the fundementals of programming session");
     
 return 0;
 }
